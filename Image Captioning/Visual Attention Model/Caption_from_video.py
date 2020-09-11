9168b260a544737363209fd1485b2e970534554f
errpv78 <parikhgoyal13@gmail.com>
2020-09-11 18:13:04 +0530

    All dependencies issues fixed, req updated

--- Image Captioning/Visual Attention Model/Caption_from_video.py
+++ Image Captioning/Visual Attention Model/Caption_from_video.py
@@ -2,35 +2,28 @@ print("Loading model from disk")
 import cv2
 import imutils
 import tensorflow as tf
-# Checking if GPU available
-physical_devices = tf.config.experimental.list_physical_devices("GPU")
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
-
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 from Predict_cap import predict_caption
 from gtts import gTTS
 import vlc
-# import pafy
-# from ffpyplayer.player import MediaPlayer
-
-# import gi
+import pafy
+from ffpyplayer.player import MediaPlayer
 
 
-# url = 'https://www.youtube.com/watch?v=NX3bSUlv4Ek'
-# vPafy = pafy.new(url)
-# play = vPafy.getbest()
+url = 'https://www.youtube.com/watch?v=NX3bSUlv4Ek'
+vPafy = pafy.new(url)
+play = vPafy.getbest()
 
 def start_video(video='city.mp4'):
     cap = cv2.VideoCapture(video)
-    # sound = MediaPlayer(play.url)
+    sound = MediaPlayer(play.url)
     # cap = cv2.VideoCapture(0)
     print("Video stream starting....")
 
     while True:
 
         ret, frame = cap.read()
-        # audio_frame, val = sound.get_frame()
+        audio_frame, val = sound.get_frame()
         if ret:
             cv2.imshow("Press c to get caption", frame)
 
@@ -48,11 +41,11 @@ def start_video(video='city.mp4'):
             break
         if key == ord("q"):
             break
-        # if val != 'eof' and audio_frame is not None:
-        #     # audio
-        #     img, t = audio_frame
+        if val != 'eof' and audio_frame is not None:
+            # audio
+            img, t = audio_frame
 
     cv2.destroyAllWindows()
     cap.release()
 
-start_video()
+start_video(play.url)
